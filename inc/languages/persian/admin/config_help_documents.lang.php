0c717c2e2844ade916087730bcc80f9c74db3b25
Abdul Samad Ghafouroi <dbookshop.ir@gmail.com>
2016-11-29 16:53:51 +0330

    Add files via upload

---
+++ inc/languages/persian/admin/config_help_documents.lang.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * MyBB 1.8.7 Persian Language Pack
+ * Copyright 2014 MyBBIran Group, All Rights Reserved
+ *
+ */
+
+$l['help_documents'] = "اسناد راهنما";
+$l['add_new_section'] = "افزودن بخش جدید‌";
+$l['add_new_section_desc'] = "در این بخش شما می‌توانید یک بخش راهنمایی تازه‌ اضافه کنید.";
+$l['add_new_document'] = "افزودن سند جدید‌";
+$l['add_new_document_desc'] = "در این بخش شما می‌توانید یک سند راهنمایی تازه‌ اضافه کنید.";
+$l['edit_section'] = "ویرایش بخش";
+$l['edit_section_desc'] = "در این بخش شما می‌توانید یک بخش راهنمایی را ویرایش کنید.";
+$l['edit_document'] = "ویرایش سند";
+$l['edit_document_desc'] = "در این بخش شما می‌توانید یک سند راهنمایی را ویرایش کنید.";
+$l['manage_help_documents'] = "مدیریت اسناد راهنما";
+$l['manage_help_documents_desc'] = "این بخش به شما اجازه‌ی مدیرت اسناد راهنماییتان را می‌دهد.";
+
+$l['title'] = "عنوان";
+$l['short_description'] = "توضیحات کوتاه";
+$l['display_order'] = "چیدمان نمایش";
+$l['enabled'] = "فعال باشد؟";
+$l['use_translation'] = "استفاده از ترجمه؟";
+$l['add_section'] = "افزودن بخش";
+$l['add_document'] = "افزودن سند";
+$l['save_section'] = "ذخیره‌ی بخش";
+$l['save_document'] = "ذخیره‌ی سند";
+$l['section'] = "بخش";
+$l['document'] = "سند";
+$l['id'] = "آی دی";
+$l['custom_doc_sec'] = "بخش/سند انتخابی";
+$l['default_doc_sec'] = "بخش/سند پیش‌فرض";
+$l['no_help_documents'] = "در حال حاضر سند راهنمایی برای انجمن شما وجود ندارد.";
+$l['section_document'] = "بخش / سند";
+
+$l['error_section_missing_name'] = "شما باید برای این بخش یک نام تعیین کنید.";
+$l['error_section_missing_description'] = "شما باید برای این بخش یک توضیح کوتاه تعیین کنید.";
+$l['error_section_missing_enabled'] = "شما باید بله یا خیر را برای \"روشن بودن\" انتخاب کنید.";
+$l['error_section_missing_translation'] = "شما باید بله یا خیر را برای \"استفاده از ترجمه\" انتخاب کنید.";
+$l['error_missing_sid'] = "شما باید برای این سند یک بخش مشخص کنید.";
+$l['error_document_missing_name'] = "شما باید یک نام برای این سند مشخص کنید.";
+$l['error_document_missing_description'] = "شما باید برای این سند یک توضیح کوتاه مشخص کنید.";
+$l['error_document_missing_document'] = "شما باید یک سند مشخص کنید.";
+$l['error_document_missing_enabled'] = "شما باید بله یا خیر را برای \"روشن بودن\" انتخاب کنید. ";
+$l['error_document_missing_translation'] = "شما باید بله یا خیر را برای \"استفاده از ترجمه\" انتخاب کنید. ";
+$l['error_invalid_sid'] = "آی دی سند مورد نظر نامعتبر است.";
+$l['error_missing_section_id'] = "بخش مورد نظر وجود ندارد.";
+$l['error_missing_hid'] = "سند مورد نظر وجود ندارد.";
+
+$l['success_help_section_added'] = "بخش راهنمایی با موفقیت افزوده شد.";
+$l['success_help_document_added'] = "سند راهنمایی با موفقیت افزوده شد.";
+$l['success_help_section_updated'] = "بخش راهنمایی با موفقیت به‌روز‌رسانی شد.";
+$l['success_help_document_updated'] = "سند راهنمایی با موفقیت به‌روز‌رسانی شد.";
+$l['success_section_deleted'] = "بخش راهنمایی انتخاب شده با موفقیت حذف شد.";
+$l['success_document_deleted'] = "سند راهنمایی انتخاب شده با موفقیت حذف شد.";
+
+$l['confirm_section_deletion'] = "آیا شما مطمئن هستید که می‌خواهید این بخش را حذف کنید؟";
+$l['confirm_document_deletion'] = "آیا شما مطمئن هستید که می‌خواهید این سند را حذف کنید؟";
+
