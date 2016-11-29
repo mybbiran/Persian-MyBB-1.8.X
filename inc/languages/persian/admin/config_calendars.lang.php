79e44eb0cc5e58b06e1be47a06c50d743adba39e
Abdul Samad Ghafouroi <dbookshop.ir@gmail.com>
2016-11-29 16:49:01 +0330

    Add files via upload

---
+++ inc/languages/persian/admin/config_calendars.lang.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * MyBB 1.8.7 Persian Language Pack
+ * Copyright 2014 MyBBIran Group, All Rights Reserved
+ *
+ */
+
+$l['calendars'] = "سالنامه‌ها";
+$l['manage_calendars'] = "مدیریت سالنامه‌ها";
+$l['manage_calendars_desc'] = "این بخش به شما اجازه‌ی مدیریت سالنامه‌های انجمن شما را می‌دهد. اگر شما چیدمان نمایش را برای یک یا چند سالنامه تغییر می‌دهید مطمئن شوید که فرم پایین صفحه را ارسال کرده‌اید. ";
+$l['add_calendar'] = "افزودن سالنامه‌ی جدید‌";
+$l['add_calendar_desc'] = "در این بخش شما می‌توانید یک سالنامه‌ی تازه‌ بسازید.";
+$l['edit_calendar'] = "ویرایش سالنامه";
+$l['edit_calendar_desc'] = "در این بخش شما می‌توانید تنظیمات این سالنامه را ویرایش کنید.";
+
+$l['calendar'] = "سالنامه";
+$l['order'] = "چیدمان نمایش";
+$l['no_calendars'] = "در حال حاضر سالنامه‌ای ‌در انجمن شما وجود ندارد.";
+$l['save_calendar_orders'] = "ذخیره‌ی چیدمان نمایش سالنامه";
+
+$l['name'] = "نام";
+$l['display_order'] = "چیدمان نمایش";
+$l['week_start'] = "آغاز هفته";
+$l['week_start_desc'] = "در این بخش شما می‌توانید روز آغازین هفته را برای این سالنامه تنظیم کنید.";
+$l['sunday'] = "یکشنبه";
+$l['monday'] = "دو‌شنبه";
+$l['tuesday'] = "سه‌شنبه";
+$l['wednesday'] = "چهار‌شنبه";
+$l['thursday'] = "پنچ‌شنبه";
+$l['friday'] = "آدینه (جمعه)";
+$l['saturday'] = "شنبه";
+$l['event_limit'] = "محدودیت رویداد";
+$l['event_limit_desc'] = "به جای آنكه‌ی تعداد رویداد‌های قبل از یك لینك نشان داده شوند تمامی رویداد در یك روز مشخص نشان داده می‌شوند.";
+$l['show_birthdays'] = "نمایش تولد‌ها؟";
+$l['show_birthdays_desc'] = "آیا شما می‌خواهید تولد کاربران عضو شده در این سالنامه نمایش داده شود؟";
+$l['moderate_events'] = "مدیریت رویداد‌های تازه‌؟";
+$l['moderate_events_desc'] = "اگر این گزینه‌ی روی بله قرار داده شود، تمامی رویداد‌های ارسال کننده کاربران به جز کاربرانی كه‌ی دسترسی \"مدیریت Bypass\" را دارند مدیریت خواهد شد.";
+$l['allow_html'] = "مجاز بودن HTML در رویداد‌ها؟";
+$l['allow_mycode'] = "مجاز بودن MyCode در رویداد‌ها؟";
+$l['allow_img'] = "مجاز بودن کد [IMG] در رویداد‌ها؟";
+$l['allow_video'] = "مجاز بودن کد [VIDEO] در رویداد‌ها؟";
+$l['allow_smilies'] = "مجاز بودن شکلک‌ها در رویداد‌ها؟";
+$l['save_calendar'] = "ذخیره‌ی سالنامه";
+
+$l['permissions'] = "مجوزهای دسترسی";
+$l['edit_permissions'] = "ویرایش دسترسی‌های سالنامه";
+$l['calendar_permissions_for'] = "دسترسی‌های سالنامه برای";
+$l['permissions_group'] = "گروه";
+$l['permissions_view'] = "نمایش";
+$l['permissions_post_events'] = "ارسال رویداد‌ها";
+$l['permissions_bypass_moderation'] = "مدیریت Bypass";
+$l['permissions_moderator'] = "مجوز دسترسی مدیر";
+$l['permissions_all'] = "همه؟";
+$l['permissions_use_group_default'] = "استفاده از دسترسی‌های پیش‌فرض گروه";
+$l['save_permissions'] = "ذخیره‌ی مجوز دسترسی";
+
+$l['error_invalid_calendar'] = "سالنامه‌ی مورد نظر وجود ندارد.";
+$l['error_missing_name'] = "شما برای این سالنامه نامی‌ وارد نکرده‌اید.";
+$l['error_missing_order'] = "شما برای این سالنامه چیدمان نمایشی وارد نکرده‌اید.";
+
+$l['success_calendar_created'] = "سالنامه با موفقیت ساخته شد.";
+$l['success_calendar_updated'] = "سالنامه با موفقیت به‌روز‌رسانی شد.";
+$l['success_calendar_permissions_updated'] = "مجوز‌های دسترسی سالنامه با موفقیت به‌روز‌رسانی شد.";
+$l['success_calendar_deleted'] = "سالنامه‌ی انتخاب شده با موفقیت حذف شد.";
+$l['success_calendar_orders_updated'] = "چیدمان نمایش سالنامه با موفقیت به‌روز‌رسانی شد.";
+
+$l['confirm_calendar_deletion'] = "آیا شما مطمئن هستید که می‌خواهید این سالنامه را حذف کنید؟";
